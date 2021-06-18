ff73f1ff5f2499a62c5eea542d0a01f9504b9379
cristian cordova <cristiancordova1997@hotmail.com>
2021-06-18 12:35:04 -0500

    tarea funciones console.

--- NTP/modulo_1/basic/HolaMundo.js
+++ NTP/modulo_1/basic/HolaMundo.js
@@ -1,1 +1,21 @@
 console.log("Hola mundo");
+//Consultar las funciones de console.
+
+console.info("Este es un console de informacion"); // Muestra un mensaje de información en la Consola Web. Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
+console.warn("advertencia");//Muestra un mensaje de advertencia. Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
+console.error("error"); // Muestra un mensaje de error. Se pueden utilizar sustituciones de cadena y argumentos adicionales con este método.
+console.clear(); //limpia la consola en si la terminal
+console.assert(); //Registra un mensaje y envía una traza de error a la consola si el primer argumento es false.
+console.count(); //Registra el numero de veces que esta línea ha sido llamada con etiqueta dada
+console.dir(); // Muestra un listado interactivo de las propiedades de un objeto JavaScript específico. Esta lista le permite examinar el contenido de los objetos hijos.
+console.group(); // Crea un nuevo grupo, indentando todos los mensajes subsecuentes en un nuevo nivel. Para retroceder un nivel, se utiliza groupEnd().
+console.groupCollapsed(); // Crea un nuevo grupo, indentando todos los mensajes subsecuentes en un nuevo nivel. A diferencia de group(), inicia con la línea de grupo colapsada, requiriendo el uso de un botón de apertura para expandir el grupo. Para retroceder un nivel, se utiliza groupEnd().
+console.groupEnd(); //Finaliza el grupo actual.
+console.dirxml(); // Muestra una representación en forma de árbol de un elemento XML/HTML si es posible o la vista del objeto JavaScript si no es posible.
+console.table(); // Muestra datos tabulares en forma de una tabla.
+console.time(); //Inicia un temporizador con un nombre específico como parámetro. Hasta 10.000 temporizadores simultáneos pueden ejecutarse en una página determinada.
+console.timeEnd(); //Detiene el temporizador especificado y registra el tiempo transcurrido, en segundos, desde su inicio.
+console.trace(); //Muestra una traza de error.
+
+
+
