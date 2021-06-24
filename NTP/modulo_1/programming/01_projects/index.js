fd1e77c72690e3ff81fec1751363f507b1e01460
cristian cordova <cristiancordova1997@hotmail.com>
2021-06-24 12:03:38 -0500

    query

--- NTP/modulo_1/programming/01_projects/index.js
+++ NTP/modulo_1/programming/01_projects/index.js
@@ -7,6 +7,30 @@ app.get('/', (req, res)=>{
     res.send("Hola mundo co ExpressJs")
 })
 
+app.get("/saludo",(req,res)=>{
+    // req.query
+    // console.log(req.query)
+    // obtenemos la query del objeto request
+    const { query: {nombre, apellido} } = req
+    // const nombre = req.query.nombre
+    // const apellido= req.query.apellido
+
+    res.json({
+        saludo: `Hola soy ${nombre} ${apellido}`
+    });
+})
+
+app.get('/saludo/:nombre', (req,res)=>{
+    
+    //const { params: { nombre } } = req;
+    const { params } = req
+    //console.log(req.params)
+    res.json({
+        nombre: params.nombre
+        //nombre: nombre
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("Sevidor escuchado en http://localhost:3000");
 })
---
+++ NTP/modulo_1/programming/01_projects/routes/index.js
@@ -0,0 +1,21 @@
+//traemos el paquete
+
+const { Router } = requiere('express');
+
+// Inicializamos Router
+const router = Router()
+
+//creacion de los endpoint
+
+
+module.exports.RouterIndex = router
+
+//module.exports.OtraCosa = route
+
+// module = {
+//     exports: {
+//         RouterIndex: router
+//     }
+// }
+
+
